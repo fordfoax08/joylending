82b4869fb1ccdd518f85538f9b8dbdcbf695867d
Jom <jadizon.2325@gmail.com>
2021-03-18 21:44:12 +0800

    UPDATE 18/3/2021 9:44 PM

--- js/dboard.js
+++ js/dboard.js
@@ -13,13 +13,15 @@ function drawPanel(e){
 
 
 /* Table Checkbox anim */
-const tableCheckBox = document.querySelectorAll(".tbody1 tr td input");
+const tableCheckBox = document.querySelectorAll(".tbody1 tr td input[type=checkbox]");
 tableCheckBox.forEach(e => {
     e.addEventListener("click",() => {
         if(e.checked){
             if(event.ctrlKey && !event.shiftKey && !event.altKey){
                 e.parentNode.style.backgroundColor = "rgb(93, 187, 93)";
             }else if(event.altKey && !event.shiftKey && !event.ctrlKey){
+                e.parentNode.style.backgroundColor = "rgb(243, 72, 72)";
+            }else if(event.shiftKey && !event.ctrlKey && !event.altKey){
                 e.parentNode.style.backgroundColor = "rgb(250, 158, 158)";
             }else{
                 e.checked = false;
@@ -34,3 +36,29 @@ tableCheckBox.forEach(e => {
     })
 });
 
+/* Show Amount in CELL */
+tableCheckBox.forEach(e => {
+    e.addEventListener("dblclick", ()=>{
+        const ammountTxt = e.parentNode.firstElementChild;
+        if(ammountTxt.classList.contains("show")){
+            ammountTxt.classList.remove("show");
+        }else{
+            ammountTxt.classList.add("show");
+        }
+    });
+});
+
+
+
+
+
+/* HELP SECTION */
+const icoHelp = document.querySelector(".ico-help");
+icoHelp.addEventListener("click",() => {
+    const helpPanel = document.querySelector(".help-container");
+    if(helpPanel.classList.contains("open")){
+        helpPanel.classList.remove("open");
+    }else{
+        helpPanel.classList.add("open");
+    }
+})
