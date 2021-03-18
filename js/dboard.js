ff611b86e2d827a1cdcbd736e61d2edd66ac2053
Yom <jadizon.2325@gmail.com>
2021-03-18 14:46:12 +0800

    UPDATE 18/03/2021

--- js/dboard.js
+++ js/dboard.js
@@ -10,3 +10,27 @@ function drawPanel(e){
         lpanel.classList.add("on");
     }
 }
+
+
+/* Table Checkbox anim */
+const tableCheckBox = document.querySelectorAll(".tbody1 tr td input");
+tableCheckBox.forEach(e => {
+    e.addEventListener("click",() => {
+        if(e.checked){
+            if(event.ctrlKey && !event.shiftKey && !event.altKey){
+                e.parentNode.style.backgroundColor = "rgb(93, 187, 93)";
+            }else if(event.altKey && !event.shiftKey && !event.ctrlKey){
+                e.parentNode.style.backgroundColor = "rgb(250, 158, 158)";
+            }else{
+                e.checked = false;
+            }
+        }else{
+            if(event.ctrlKey && event.shiftKey){
+                e.parentNode.removeAttribute("style");
+            }else{
+                e.checked = true;
+            }
+        }
+    })
+});
+
